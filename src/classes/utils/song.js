e4253b4b28bcca2a63d9cb96f949e5275cb9dbbc
Timmy Kokke <mental_c@hotmail.com>
2019-09-03 13:43:38 +0200

    added start of music

---
+++ src/classes/utils/song.js
@@ -0,0 +1,96 @@
+    // This music has been exported by SoundBox. You can use it with
+    // http://sb.bitsnbites.eu/player-small.js in your own product.
+
+    // See http://sb.bitsnbites.eu/demo.html for an example of how to
+    // use it in a demo.
+
+    // Song data
+export const song = {
+        songData: [
+          { // Instrument 0
+            i: [
+            1, // OSC1_WAVEFORM
+            192, // OSC1_VOL
+            128, // OSC1_SEMI
+            0, // OSC1_XENV
+            1, // OSC2_WAVEFORM
+            191, // OSC2_VOL
+            116, // OSC2_SEMI
+            9, // OSC2_DETUNE
+            0, // OSC2_XENV
+            0, // NOISE_VOL
+            6, // ENV_ATTACK
+            22, // ENV_SUSTAIN
+            34, // ENV_RELEASE
+            0, // ARP_CHORD
+            0, // ARP_SPEED
+            2, // LFO_WAVEFORM
+            127, // LFO_AMT
+            2, // LFO_FREQ
+            1, // LFO_FX_FREQ
+            2, // FX_FILTER
+            118, // FX_FREQ
+            72, // FX_RESONANCE
+            0, // FX_DIST
+            32, // FX_DRIVE
+            77, // FX_PAN_AMT
+            6, // FX_PAN_FREQ
+            25, // FX_DELAY_AMT
+            6 // FX_DELAY_TIME
+            ],
+            // Patterns
+            p: [1,2],
+            // Columns
+            c: [
+              {n: [139,,146,,139,,146,,139,,146,,139,,146,,139,,146,,139,,146,,139,,146,,139,,146,,137,,144,,137,,144,,137,,144,,137,,144,,137,,144,,137,,144,,137,,144,,137,,144,,,142,,149,,142,,149,,142,,149,,142,,149,,142,,149,,142,,149,,142,,149,,142,,149,,141,,147,,141,,147,,141,,147,,141,,147,,141,,147,,141,,147,,141,,147,,141,,147],
+               f: []},
+              {n: [],
+               f: []}
+            ]
+          },
+          { // Instrument 1
+            i: [
+            2, // OSC1_WAVEFORM
+            100, // OSC1_VOL
+            128, // OSC1_SEMI
+            0, // OSC1_XENV
+            3, // OSC2_WAVEFORM
+            201, // OSC2_VOL
+            128, // OSC2_SEMI
+            0, // OSC2_DETUNE
+            0, // OSC2_XENV
+            0, // NOISE_VOL
+            5, // ENV_ATTACK
+            6, // ENV_SUSTAIN
+            58, // ENV_RELEASE
+            0, // ARP_CHORD
+            0, // ARP_SPEED
+            0, // LFO_WAVEFORM
+            195, // LFO_AMT
+            6, // LFO_FREQ
+            1, // LFO_FX_FREQ
+            2, // FX_FILTER
+            135, // FX_FREQ
+            0, // FX_RESONANCE
+            0, // FX_DIST
+            32, // FX_DRIVE
+            147, // FX_PAN_AMT
+            6, // FX_PAN_FREQ
+            121, // FX_DELAY_AMT
+            6 // FX_DELAY_TIME
+            ],
+            // Patterns
+            p: [1],
+            // Columns
+            c: [
+              {n: [115,,115,,115,,115,,115,,127,,115,,115,,115,,115,,115,,127,,115,,115,,127,,115,,113,,113,,113,,113,,113,,125,,113,,113,,113,,113,,113,,125,,113,,113,,125,,113],
+               f: []}
+            ]
+          },
+        ],
+        rowLen: 8269,   // In sample lengths
+        patternLen: 64,  // Rows per pattern
+        endPattern: 1,  // End pattern
+        numChannels: 2  // Number of channels
+      };
+  
