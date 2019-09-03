11ea4043111eccac73d6876ff16699195f707a1e
Timmy Kokke <mental_c@hotmail.com>
2019-09-04 00:20:59 +0200

    fixed model, now looking at player.

--- src/classes/components/game.component.js
+++ src/classes/components/game.component.js
@@ -7,23 +7,32 @@ const wave = [[
     [0, 4, 4, 4, 0, 0, 0, 4, 4, 4, 0],
     [0, 4, 0, 4, 0, 0, 0, 4, 0, 4, 0]],
 [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]]
+    [0, 2, 0, 0, 0, 3, 0, 0, 0, 2, 0],
+    [0, 4, 0, 0, 0, 1, 0, 0, 0, 4, 0],
+    [4, 2, 4, 0, 1, 3, 1, 0, 4, 2, 4],
+    [2, 4, 2, 0, 3, 1, 3, 0, 2, 4, 2],
+    [4, 2, 4, 0, 1, 3, 1, 0, 4, 2, 4]],
+[
+    [0, 0, 0, 3, 2, 1, 2, 3, 0, 0, 0],
+    [0, 0, 3, 2, 1, 1, 1, 2, 3, 0, 0],
+    [0, 3, 2, 1, 1, 4, 1, 1, 2, 3, 0],
+    [0, 0, 3, 2, 1, 1, 1, 2, 3, 0, 0],
+    [0, 0, 0, 3, 2, 1, 2, 3, 0, 0, 0]],
+[
+    [1, 2, 3, 4, 4, 4, 4, 4, 3, 2, 1],
+    [0, 1, 2, 3, 4, 4, 4, 3, 2, 1, 0],
+    [0, 0, 1, 2, 3, 4, 3, 2, 1, 0, 0],
+    [0, 0, 0, 1, 2, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0]]]
 
 export default AFRAME.registerComponent('game', {
     schema: {
-        spawninterval: { default: 1000 },
         currentwave: { default: 0 }
     },
 
     init: function () {
         this.gameover = false;
         this.gameState = 0;// Title-Screen -> 1 = Playing -> 2 = Game-Over -> 1
-
-        this.spawntimer = this.data.spawninterval;
         this.invadergroup = document.getElementById("invaders");
         this.missilegroup = document.getElementById("missiles");
 
@@ -31,7 +40,7 @@ export default AFRAME.registerComponent('game', {
             switch (this.gameState) {
                 case 0:
                     document.getElementById('titlescreen')
-                        .setAttribute('animation','property: position; to: 0 -5 0; dur: 1500; easing: easeOutCubic');
+                        .setAttribute('animation', 'property: position; to: 0 -5 0; dur: 1500; easing: easeOutCubic');
                     this.gameState = 1;
                     this.invadersLeftInWave = this.spawnInvaderWave();
                     break;
@@ -53,6 +62,12 @@ export default AFRAME.registerComponent('game', {
 
             e.detail.missile.remove();
             e.detail.invader.remove();
+
+            this.invadersLeftInWave -= 1;
+            if (this.invadersLeftInWave == 0) {
+                this.data.currentwave = (this.data.currentwave + 1) % wave.length;
+                this.invadersLeftInWave = this.spawnInvaderWave();
+            }
         })
 
         this.el.addEventListener('game-over', () => {
@@ -74,10 +89,12 @@ export default AFRAME.registerComponent('game', {
         for (let i = 0; i < 5; i++) { // wave rows
             for (let j = 0; j < 11; j++) { // wave columns
                 if (wave[this.data.currentwave][i][j] > 0) {
+                    spawned++;
                     this.spawnInvader(j, i, wave[this.data.currentwave][i][j])
                 }
             }
         }
+        return spawned;
     },
 
     spawnInvader: function (x, y, type) {
