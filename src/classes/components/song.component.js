16b7ef6c068044a334d033bacb7428e7a2a5f659
Timmy Kokke <mental_c@hotmail.com>
2020-04-09 10:28:07 +0200

    Converted song to mp3 to increase loading time

--- src/classes/components/song.component.js
+++ src/classes/components/song.component.js
@@ -1,6 +1,3 @@
-import { CPlayer } from '../utils/player';
-import { song } from '../utils/song';
-
 export default AFRAME.registerElement('a-asset-song', {
     prototype: Object.create(AFRAME.ANode.prototype, {
         createdCallback: {
@@ -11,45 +8,19 @@ export default AFRAME.registerElement('a-asset-song', {
 
         attachedCallback: {
             value: function () {
-                var player = new CPlayer();
-                player.init(song);
-               // Generate music...
-                var done = false;
-                while(!done){
-                    done = player.generate() >= 1;
-                    if (done) {
-                        var wave = player.createWave();
+  
                         var audio = document.createElement("audio");
                         audio.volume = 0.2;
                         audio.addEventListener('ended', function() {
                             this.currentTime = 0;
                             this.play();
                         }, false);
-                        audio.src = URL.createObjectURL(new Blob([wave], { type: "audio/wav" }));
+
+                        audio.src = 'BackToSpaceMusic192.mp3';
                         document.querySelector('a-scene').setAttribute('visible','true');
                         audio.play();
-                    }
-                };
+
             }
         }
     })
 });
-
-// var t0 = new Date();
-// var player = new CPlayer();
-// player.init(song);
-
-// // Generate music...
-// var done = false;
-// setInterval(function () {
-//     if (done) {
-//         return;
-//     }
-//     done = player.generate() >= 1;
-//     if (done) {
-//         var wave = player.createWave();
-//         var audio = document.createElement("audio");
-//         audio.src = URL.createObjectURL(new Blob([wave], { type: "audio/wav" }));
-//         //   audio.play();
-//     }
-// }, 0);
